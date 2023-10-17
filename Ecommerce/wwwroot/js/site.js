bf5e3356b96e34717e989d6dcc4ec3d8e1f448fc
MarcoCaamal <caamalmarco99@gmail.com>
2023-10-16 23:00:54 -0500

    Advance of navbar

--- Ecommerce/wwwroot/js/site.js
+++ Ecommerce/wwwroot/js/site.js
@@ -1,4 +1,32 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+});
 
-// Write your JavaScript code.
+function initApp() {
+    initMenuBtn();
+}
+
+function initMenuBtn() {
+    const menuBtn = document.querySelector('i.menu__btn');
+    const menuBtnX = document.querySelector('i.menu__btn--x');
+
+    const btnCategories = document.querySelector('.categories');
+    const searchBar = document.querySelector('.search-bar');
+    const navbar = document.querySelector('.navbar');
+
+    menuBtn.addEventListener('click', () => {
+        menuBtn.style.display = 'none';
+        menuBtnX.style.display = 'block';
+        btnCategories.classList.remove('d-none');
+        searchBar.classList.remove('d-none');
+        navbar.classList.remove('d-none');
+    });
+
+    menuBtnX.addEventListener('click', () => {
+        menuBtn.style.display = 'block';
+        menuBtnX.style.display = 'none';
+        btnCategories.classList.add('d-none');
+        searchBar.classList.add('d-none');
+        navbar.classList.add('d-none');
+    });
+}
