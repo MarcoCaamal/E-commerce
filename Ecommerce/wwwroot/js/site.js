5c71a348d6779a582b6760c02b6f79ffcd701dbe
MarcoCaamal <caamalmarco99@gmail.com>
2024-03-29 22:48:04 -0500

    Feat, Barra de navegacion terminada

--- Ecommerce/wwwroot/js/site.js
+++ Ecommerce/wwwroot/js/site.js
@@ -2,31 +2,51 @@
     initApp();
 });
 
+/* GLOBAL */
+const menuBtn = document.querySelector('i.menu__btn');
+const menuBtnX = document.querySelector('i.menu__btn--x');
+
+const navbarResponsive = document.querySelector('.bar__responsive-menu');
+
 function initApp() {
     initMenuBtn();
+    addResizeEvent();
+}
+
+function addResizeEvent() {
+    window.addEventListener('resize', (e) => {
+        if (e.target.innerWidth == 768) {
+            menuBtn.style.display = 'none';
+            menuBtnX.style.display = 'none';
+
+        }  
+
+        if (e.target.innerWidth < 768) {
+            menuBtn.style.display = null;
+            menuBtnX.style.display = null;
+
+            if (!navbarResponsive.classList.contains('mostrar')) return;
+
+            navbarResponsive.classList.remove('mostrar');
+        }
+
+        
+    });
 }
 
 function initMenuBtn() {
-    const menuBtn = document.querySelector('i.menu__btn');
-    const menuBtnX = document.querySelector('i.menu__btn--x');
-
-    const btnCategories = document.querySelector('.categories');
-    const searchBar = document.querySelector('.search-bar');
-    const navbar = document.querySelector('.navbar');
-
     menuBtn.addEventListener('click', () => {
         menuBtn.style.display = 'none';
         menuBtnX.style.display = 'block';
-        btnCategories.classList.remove('d-none');
-        searchBar.classList.remove('d-none');
-        navbar.classList.remove('d-none');
+        showOrHiddenMenuResponsive();
     });
-
     menuBtnX.addEventListener('click', () => {
         menuBtn.style.display = 'block';
         menuBtnX.style.display = 'none';
-        btnCategories.classList.add('d-none');
-        searchBar.classList.add('d-none');
-        navbar.classList.add('d-none');
+        showOrHiddenMenuResponsive();
     });
+}
+
+function showOrHiddenMenuResponsive() {
+    navbarResponsive.classList.toggle('mostrar');
 }
